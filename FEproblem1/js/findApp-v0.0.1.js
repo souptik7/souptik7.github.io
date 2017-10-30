f25b0f636e7adf55c6394263a0c0ad9f31297214
souptik7 <souptik@msupply.com>
2017-10-30 16:11:29 +0530

    radio and dropdown completed

--- FEproblem1/js/findApp-v0.0.1.js
+++ FEproblem1/js/findApp-v0.0.1.js
@@ -31,10 +31,26 @@ findApp.controller('mainController', function($scope, $http, $window, $location,
     });
 
     $scope.serivceURL = URL.serivceURL;
-    $scope.find = {};
 
     $scope.load = function(){
         $rootScope.loading = true;
+        $scope.find = {};
+        $scope.find.planet = [];
+        $scope.find.vehicle = [];
+        $scope.vehiclesRadio = [];
+        $scope.oldModal = [];
+        $scope.oldModalKey = [];
+        for(var i=0;i<4;i++){
+            var tempObj = {
+                "name":"vehicle"+i,
+                "ids":[]
+            };
+            for(var j=0;j<4;j++){
+                tempObj.ids.push("vehicle"+i+j);
+            }
+            $scope.vehiclesRadio.push(tempObj);
+        }
+        $scope.showVehicles = [false,false,false,false];
         var data = {};
         data = JSON.stringify(data);
         $http.post($scope.serivceURL + 'token', data, {
@@ -46,6 +62,7 @@ findApp.controller('mainController', function($scope, $http, $window, $location,
         });
         $http.get($scope.serivceURL + 'planets')
         .then(function(data){
+            $scope.mainPlanets = data.data;
             $scope.planets = [];
             for(var i=0;i<4;i++){
                 $scope.planets.push(data.data);
@@ -53,6 +70,7 @@ findApp.controller('mainController', function($scope, $http, $window, $location,
         })
         $http.get($scope.serivceURL + 'vehicles')
         .then(function(data){
+            $scope.mainVehicles = data.data;
             $scope.vehicles = [];
             for(var i=0;i<4;i++){
                 $scope.vehicles.push(data.data);
@@ -63,11 +81,45 @@ findApp.controller('mainController', function($scope, $http, $window, $location,
 
     $scope.load();
 
-    // $scope.sortArray = function(key){
-    //     if($scope.find.planet[key] == undefined)return;
-    //     for(var i=0;i<$scope.planets[key].length;i++){
-            
-    //     }
-    // }
+    $scope.getPlanets = function(key){
+        if($scope.find.planet[key] == undefined)return;
+        for(var i=0;i<4;i++){
+            if(key != i){
+                var tempArr1 = [];
+                var loops = $scope.mainPlanets.length;
+                for(var j=0;j<loops;j++){
+                    var count = 0;
+                    for(var k=0;k<4;k++){
+                        if($scope.find.planet[k] != null && k != i && JSON.parse($scope.find.planet[k]).name == $scope.mainPlanets[j].name){
+                            count++;
+                        }
+                    }
+                    if(count == 0){
+                        tempArr1.push($scope.mainPlanets[j]);
+                    }
+                }
+                $scope.planets[i] = tempArr1;
+            }
+            else {
+                $scope.showVehicles[i] = true;
+            }
+        }
+    }
+
+    $scope.getVehicles = function(key1,key2){
+        for(var i=0;i<1;i++){
+            $scope.vehicles[i][key2].total_no = $scope.vehicles[i][key2].total_no - 1;
+            if($scope.oldModal[key1] != undefined && $scope.oldModal[key1] != $scope.find.vehicle[key1]){
+                $scope.vehicles[i][$scope.oldModalKey[key1]].total_no = $scope.vehicles[i][$scope.oldModalKey[key1]].total_no + 1;
+            }
+        }
+        $scope.oldModalKey[key1] = key2;
+        $scope.oldModal[key1] = $scope.find.vehicle[key1];
+        computeTime();
+    }
+
+    function computeTime(){
+        
+    }
 
 });
