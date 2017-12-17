528cc4266f07af98ab8bd4d348f3509234d6d563
babai1203 <souptik.developer7@gmail.com>
2017-12-18 01:07:54 +0530

    checking

--- DocsApp/js/controllers/chatbox-v0.0.1.js
+++ DocsApp/js/controllers/chatbox-v0.0.1.js
@@ -12,9 +12,18 @@ docsApp.registerCtrl('chatController', function($scope, $http, $location, $rootS
     	// 	'serial':
     	// }
     	// $scope.chatData.push(data);
-    	$http.get($scope.serivceURL + '?apiKey=' + $scope.apiKey + '&message=' + $scope.chat.messsage + '&chatBotID=' + $scope.chatBotID + '&externalID=' + $scope.externalID)
-    	.then(function(data){
-    		console.log(data);
-    	});
+    	// $http.get($scope.serivceURL + '?apiKey=' + $scope.apiKey + '&message=' + $scope.chat.messsage + '&chatBotID=' + $scope.chatBotID + '&externalID=' + $scope.externalID)
+    	// .then(function(data){
+    	// 	console.log(data);
+    	// });
+        var url = $scope.serivceURL + '?apiKey=' + $scope.apiKey + '&message=' + $scope.chat.messsage + '&chatBotID=' + $scope.chatBotID + '&externalID=' + $scope.externalID;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+               console.log(http.responseText);
+            }
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
     }
 });
